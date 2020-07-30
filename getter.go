e7cc3fe518c20df17194961eb4fe80c4eb706d97
Konstantin Voykov <koykov0@gmail.com>
2020-07-31 00:33:04 +0300

    Add support of getter callback functions.

---
+++ getter.go
@@ -0,0 +1,21 @@
+package jsondecoder
+
+type GetterFn func(ctx *Ctx, buf *interface{}, args []interface{}) error
+
+var (
+	getterRegistry = map[string]GetterFn{}
+)
+
+func RegisterGetterFn(name, alias string, cb GetterFn) {
+	getterRegistry[name] = cb
+	if len(alias) > 0 {
+		getterRegistry[alias] = cb
+	}
+}
+
+func GetGetterFn(name string) *GetterFn {
+	if fn, ok := getterRegistry[name]; ok {
+		return &fn
+	}
+	return nil
+}
---
+++ getter_builtin.go
@@ -0,0 +1,6 @@
+package jsondecoder
+
+func getterCrc32(ctx *Ctx, buf *interface{}, args []interface{}) error {
+	// todo implement me
+	return nil
+}
