995304916639d3726c93003bcfc15ef84c9b2093
Konstantin Voykov <koykov0@gmail.com>
2020-08-13 23:14:13 +0300

    Add context pool.

---
+++ ctx_pool.go
@@ -0,0 +1,39 @@
+package jsondecoder
+
+import "sync"
+
+// Context pool.
+type CtxPool struct {
+	p sync.Pool
+}
+
+// Default instance of context pool.
+// You may use it directly as jsondecoder.CP.Get()/Put() or using functions AcquireCtx()/ReleaseCtx().
+var CP CtxPool
+
+// Get context object from the pool or make new object if pool is empty.
+func (p *CtxPool) Get() *Ctx {
+	v := p.p.Get()
+	if v != nil {
+		if c, ok := v.(*Ctx); ok {
+			return c
+		}
+	}
+	return NewCtx()
+}
+
+// Put the object to the pool.
+func (p *CtxPool) Put(ctx *Ctx) {
+	ctx.Reset()
+	p.p.Put(ctx)
+}
+
+// Get object from the default context pool.
+func AcquireCtx() *Ctx {
+	return CP.Get()
+}
+
+// Put object back to default pool.
+func ReleaseCtx(ctx *Ctx) {
+	CP.Put(ctx)
+}
