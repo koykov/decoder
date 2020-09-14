ea7733a096b3d643e0164e0b44bf1ea31479dad8
Konstantin Voykov <koykov0@gmail.com>
2020-09-14 22:55:46 +0300

    Comment ctx code.

--- ctx.go
+++ ctx.go
@@ -7,31 +7,36 @@ import (
 	"github.com/koykov/jsonvector"
 )
 
+// Context object. Contains list of variables that can be used as source or destination.
 type Ctx struct {
+	// List of context variables and list len.
 	vars []ctxVar
 	ln   int
-
+	// JSON vector objects list and list len.
 	p  []*jsonvector.Vector
 	pl int
-
+	// Internal buffers.
 	buf  []byte
 	bufS []string
 	bufI int
 	bufX interface{}
 	bufA []interface{}
-
+	// External buffers to use in modifier and and other callbacks.
 	Buf, Buf1, Buf2 bytealg.ChainBuf
 
 	Err error
 }
 
+// Context variable object.
 type ctxVar struct {
 	key string
 	val interface{}
 	ins inspector.Inspector
+	// Var is JSON node case.
 	jsn *jsonvector.Node
 }
 
+// Make new context object.
 func NewCtx() *Ctx {
 	ctx := Ctx{
 		vars: make([]ctxVar, 0),
@@ -41,6 +46,8 @@ func NewCtx() *Ctx {
 	return &ctx
 }
 
+// Set the variable to context.
+// Inspector ins should be correspond to variable val.
 func (c *Ctx) Set(key string, val interface{}, ins inspector.Inspector) {
 	for i := 0; i < c.ln; i++ {
 		if c.vars[i].key == key {
@@ -69,6 +76,7 @@ func (c *Ctx) Set(key string, val interface{}, ins inspector.Inspector) {
 	c.ln++
 }
 
+// Set static variable to context.
 func (c *Ctx) SetStatic(key string, val interface{}) {
 	ins, err := inspector.GetInspector("static")
 	if err != nil {
@@ -78,6 +86,7 @@ func (c *Ctx) SetStatic(key string, val interface{}) {
 	c.Set(key, val, ins)
 }
 
+// Parse data as JSON source and set it to context as key.
 func (c *Ctx) SetJson(key string, data []byte) (vec *jsonvector.Vector, err error) {
 	vec = c.getParser()
 	if err = vec.Parse(data); err != nil {
@@ -88,6 +97,7 @@ func (c *Ctx) SetJson(key string, data []byte) (vec *jsonvector.Vector, err erro
 	return
 }
 
+// Directly set node to context as key.
 func (c *Ctx) SetJsonNode(key string, node *jsonvector.Node) error {
 	if node == nil {
 		return ErrEmptyNode
@@ -118,29 +128,52 @@ func (c *Ctx) SetJsonNode(key string, node *jsonvector.Node) error {
 	return nil
 }
 
+// Get arbitrary value from the context by path.
+//
+// See Ctx.get().
+// Path syntax: <ctxVrName>[.<Field>[.<NestedField0>[....<NestedFieldN>]]]
+// Examples:
+// * user.Bio.Birthday
+// * staticVar
 func (c *Ctx) Get(path string) interface{} {
 	return c.get(fastconv.S2B(path), nil)
 }
 
+// Internal getter.
 func (c *Ctx) get(path []byte, subset [][]byte) interface{} {
 	if len(path) == 0 {
 		return nil
 	}
+
+	// Split path to separate words using dot as separator.
+	// So, path user.Bio.Birthday will convert to []string{"user", "Bio", "Birthday"}
 	c.bufS = c.bufS[:0]
 	c.bufS = bytealg.AppendSplitStr(c.bufS, fastconv.B2S(path), ".", -1)
+	if len(c.bufS) == 0 {
+		return nil
+	}
+
+	// Look for first path chunk in vars.
 	for i, v := range c.vars {
 		if i == c.ln {
+			// Vars limit reached, exit.
 			break
 		}
 		if v.key == c.bufS[0] {
+			// Var found.
 			if v.jsn != nil {
+				// Var is JSON node.
 				if len(subset) > 0 {
+					// List of subsets provided.
+					// Preserve item in []str buffer to check each key separately.
 					c.bufS = append(c.bufS, "")
 					for _, tail := range subset {
 						if len(tail) > 0 {
+							// Fill preserved item with subset's value.
 							c.bufS[len(c.bufS)-1] = fastconv.B2S(tail)
 							c.bufX = v.jsn.Get(c.bufS[1:]...)
 							if n, ok := c.bufX.(*jsonvector.Node); ok && n != nil {
+								// Successful hunt.
 								break
 							}
 						}
@@ -151,6 +184,7 @@ func (c *Ctx) get(path []byte, subset [][]byte) interface{} {
 				return c.bufX
 			}
 			if v.ins != nil {
+				// Variable is covered by inspector.
 				c.Err = v.ins.GetTo(v.val, &c.bufX, c.bufS[1:]...)
 				if c.Err != nil {
 					return nil
@@ -163,6 +197,9 @@ func (c *Ctx) get(path []byte, subset [][]byte) interface{} {
 	return nil
 }
 
+// Internal setter.
+//
+// Set val to destination by address path.
 func (c *Ctx) set(path []byte, val interface{}) error {
 	if len(path) == 0 {
 		return nil
@@ -187,6 +224,7 @@ func (c *Ctx) set(path []byte, val interface{}) error {
 	return nil
 }
 
+// Get new JSON vector object from the context buffer.
 func (c *Ctx) getParser() *jsonvector.Vector {
 	var v *jsonvector.Vector
 	if c.pl < len(c.p) {
@@ -199,6 +237,9 @@ func (c *Ctx) getParser() *jsonvector.Vector {
 	return v
 }
 
+// Reset the context.
+//
+// Made to use together with pools.
 func (c *Ctx) Reset() {
 	for i := 0; i < c.ln; i++ {
 		c.vars[i].jsn = nil
