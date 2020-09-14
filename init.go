8054a140dc38f53da1400fa269fa81f1b29cb9a5
Konstantin Voykov <koykov0@gmail.com>
2020-09-14 22:59:46 +0300

    Comment init code.

--- init.go
+++ init.go
@@ -3,13 +3,17 @@ package decoder
 import "github.com/koykov/inspector"
 
 func init() {
+	// Register builtin modifiers.
 	RegisterModFn("default", "def", modDefault)
 
+	// Register builtin getter callbacks.
 	RegisterGetterFn("crc32", "", getterCrc32)
 	RegisterGetterFn("appendTestHistory", "", getterAppendTestHistory)
 
+	// Register builtin callbacks.
 	RegisterCallbackFn("foo", "", cbFoo)
 	RegisterCallbackFn("jsonParseAs", "jsonParse", cbJsonParse)
 
+	// Register assign functions.
 	inspector.RegisterAssignFn(AssignJsonNode)
 }
