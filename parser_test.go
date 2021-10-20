029f456976ea1b4190f8e61b9ab49a3f9c9b313c
Konstantin Voykov <koykov0@gmail.com>
2021-10-20 17:57:43 +0300

    Use common parser test func.

--- parser_test.go
+++ parser_test.go
@@ -5,25 +5,13 @@ import (
 	"testing"
 )
 
-func TestParserV2V(t *testing.T) {
+func TestParser(t *testing.T) {
 	t.Run("v2v0", func(t *testing.T) { testParser(t) })
 	t.Run("v2v1", func(t *testing.T) { testParser(t) })
 	t.Run("v2v2", func(t *testing.T) { testParser(t) })
-}
-
-func TestParserF2V(t *testing.T) {
 	t.Run("f2v0", func(t *testing.T) { testParser(t) })
-}
-
-func TestParserV2C(t *testing.T) {
 	t.Run("v2c0", func(t *testing.T) { testParser(t) })
-}
-
-func TestParserV2CI(t *testing.T) {
 	t.Run("v2ci0", func(t *testing.T) { testParser(t) })
-}
-
-func TestParserCb(t *testing.T) {
 	t.Run("cb0", func(t *testing.T) { testParser(t) })
 }
 
