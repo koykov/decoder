6b6b19f4eb108782896413598f845561870ffc73
Konstantin Voykov <koykov0@gmail.com>
2021-10-21 14:10:42 +0300

    Cover mod cases with new testing system (benchmark).

--- mod_test.go
+++ mod_test.go
@@ -17,6 +17,21 @@ func testMod(t *testing.T, jsonKey string, assertFn func(t testing.TB, obj *test
 	assertFn(t, obj)
 }
 
+func BenchmarkMod(b *testing.B) {
+	b.Run("default", func(b *testing.B) { benchMod(b, "src", scenarioModDefault) })
+}
+
+func benchMod(b *testing.B, jsonKey string, assertFn func(t testing.TB, obj *testobj.TestObject)) {
+	ctx := NewCtx()
+	obj := &testobj.TestObject{}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		obj = assertDecode(b, ctx, obj, "mod", jsonKey)
+		assertFn(b, obj)
+		obj.Clear()
+	}
+}
+
 func scenarioModDefault(t testing.TB, obj *testobj.TestObject) {
 	assertB(t, "Name", obj.Name, []byte("Marquis Warren"))
 	assertI32(t, "Status", obj.Status, 1)
