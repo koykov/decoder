9af55133f1880637ac9c9dd4488afed2f0240410
Konstantin Voykov <koykov0@gmail.com>
2021-10-21 12:49:04 +0300

    Cover decoder cases with new testing system.

--- common_test.go
+++ common_test.go
@@ -1,11 +1,15 @@
 package decoder
 
 import (
+	"bytes"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/koykov/inspector/testobj"
+	"github.com/koykov/inspector/testobj_ins"
 )
 
 type stage struct {
@@ -14,10 +18,14 @@ type stage struct {
 }
 
 var (
-	stages []stage
+	stages  []stage
+	jsonSrc map[string][]byte
+	buf     []byte
 )
 
 func init() {
+	jsonSrc = make(map[string][]byte)
+
 	_ = filepath.Walk("testdata/parser", func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == ".dec" {
 			st := stage{}
@@ -31,6 +39,25 @@ func init() {
 				st.expect = raw
 			}
 			stages = append(stages, st)
+		}
+		return nil
+	})
+	_ = filepath.Walk("testdata/decoder", func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".dec" {
+			st := stage{}
+			st.key = strings.Replace(filepath.Base(path), ".dec", "", 1)
+			st.key = "decoder/" + st.key
+
+			st.origin, _ = ioutil.ReadFile(path)
+			rules, _ := Parse(st.origin)
+			RegisterDecoder(st.key, rules)
+
+			stages = append(stages, st)
+		}
+		if filepath.Ext(path) == ".json" {
+			key := strings.Replace(filepath.Base(path), ".json", "", 1)
+			src, _ := ioutil.ReadFile(path)
+			jsonSrc[key] = src
 		}
 		return nil
 	})
@@ -49,4 +76,55 @@ func getStage(key string) (st *stage) {
 func getTBName(tb testing.TB) string {
 	key := tb.Name()
 	return key[strings.Index(key, "/")+1:]
+}
+
+func assertDecode(t testing.TB, ctx *Ctx, obj *testobj.TestObject, jsonKey string) *testobj.TestObject {
+	ctx.Reset()
+	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
+	buf = append(buf[:0], jsonSrc[jsonKey]...)
+	_, err := ctx.SetVector("jso", buf, VectorJson)
+	if err != nil {
+		t.Error(err)
+	}
+	key := "decoder/" + getTBName(t)
+	err = Decode(key, ctx)
+	if err != nil {
+		t.Error(err)
+	}
+	return obj
+}
+
+func assertS(t testing.TB, field, val, expect string) {
+	if val != expect {
+		key := getTBName(t)
+		t.Errorf("%s %s test failed", key, field)
+	}
+}
+
+func assertB(t testing.TB, field string, val, expect []byte) {
+	if !bytes.Equal(val, expect) {
+		key := getTBName(t)
+		t.Errorf("%s %s test failed", key, field)
+	}
+}
+
+func assertI32(t testing.TB, field string, val, expect int32) {
+	if val != expect {
+		key := getTBName(t)
+		t.Errorf("%s %s test failed", key, field)
+	}
+}
+
+func assertF64(t testing.TB, field string, val, expect float64) {
+	if val != expect {
+		key := getTBName(t)
+		t.Errorf("%s %s test failed", key, field)
+	}
+}
+
+func assertB1(t testing.TB, field string, val, expect bool) {
+	if val != expect {
+		key := getTBName(t)
+		t.Errorf("%s %s test failed", key, field)
+	}
 }
--- decoder_test.go
+++ decoder_test.go
@@ -1,16 +1,12 @@
 package decoder
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/koykov/inspector/testobj"
-	"github.com/koykov/inspector/testobj_ins"
 )
 
 var (
-	buf []byte
-
 	decTestSrc = []byte(`{
   "identifier": "xf44e",
   "person": {
@@ -29,47 +25,18 @@ var (
     "perm": [false, true, true]
   }
 }`)
-	decTestNestedJsonSrc = []byte(`{
-	"id":"xFF45",
-	"nickname":"Chris Mannix",
-	"prop":"{\"id\":1,\"name\":\"Foo\",\"price\":123,\"tags\":[\"Bar\",\"Eek\"],\"stock\":{\"warehouse\":300,\"retail\":20}}"
-}`)
-
-	decTest0 = []byte(`obj.Id = jso.identifier
-obj.Name = jso.person.full_name
-obj.Status = jso.person.status
-obj.Cost = jso.finance.balance
-obj.Finance.AllowBuy = jso.finance.is_active
-obj.Flags[read] = jso.person.read_f
-obj.Flags[write] = jso.person.write_f
-obj.Permission[45] = jso.ext.perm[0]
-obj.Permission[59] = jso.ext.perm[2]`)
-	decTest1 = []byte(`obj.Id = jso.identifier
-obj.Name = jso.person.full_name
-obj.Status = jso.person.status
-obj.Cost = jso.finance.balance
-obj.Finance.AllowBuy = jso.finance.is_active
-obj.Finance.MoneyIn = 15.4532
-obj.Finance.MoneyOut = jso.person.last_buy
-obj.Finance.Balance = jso.finance.balance_total`)
-	decTest2 = []byte(`obj.Finance.History = appendTestHistory(obj.Finance.History, 13.1415, jso.person.full_name)
-obj.Finance.History = appendTestHistory(obj.Finance.History, jso.finance.balance, "foobar")`)
-	decTest3 = []byte(`obj.Id = jso.id
-obj.Name = jso.nickname
-jsonParseAs(jso.prop, "properties")
-obj.Cost = properties.price`)
-	decTest4    = []byte(`obj.Name = crc32(jso.person.{id|nickname|full_name})`)
-	crc32Expect = []byte(`2677594116`)
 )
+
+func TestDecoder(t *testing.T) {
+	t.Run("decoder0", func(t *testing.T) { testDecoder(t, "src", scenario0) })
+	t.Run("decoder1", func(t *testing.T) { testDecoder(t, "src", scenario1) })
+	t.Run("decoder2", func(t *testing.T) { testDecoder(t, "src", scenario2) })
+	t.Run("decoder3", func(t *testing.T) { testDecoder(t, "srcNested", scenario3) })
+	t.Run("decoder4", func(t *testing.T) { testDecoder(t, "src", scenario4) })
+}
 
 func pretest(t testing.TB) {
 	dec := map[string][]byte{
-		"decTest0": decTest0,
-		"decTest1": decTest1,
-		"decTest2": decTest2,
-		"decTest3": decTest3,
-		"decTest4": decTest4,
-
 		"decModDefault0": decModDefault0,
 	}
 	for id, body := range dec {
@@ -82,255 +49,66 @@ func pretest(t testing.TB) {
 	}
 }
 
-func assertTest1(t testing.TB, obj *testobj.TestObject) {
-	if obj.Id != "xf44e" {
-		t.Error("decode 0 id test failed")
-	}
-	if !bytes.Equal(obj.Name, []byte("Marquis Warren")) {
-		t.Error("decode 0 name test failed")
-	}
-	if obj.Status != 67 {
-		t.Error("decode 0 status test failed")
-	}
-	if obj.Cost != 164.5962 {
-		t.Error("decode 0 cost test failed")
-	}
-	if obj.Finance.AllowBuy != true {
-		t.Error("decode 0 finance.allowBuy test failed")
-	}
-	if obj.Finance.MoneyIn != 15.4532 {
-		t.Error("decode 0 finance.moneyIn test failed")
-	}
-	if obj.Finance.MoneyOut != 45.90421 {
-		t.Error("decode 0 finance.moneyOut test failed")
-	}
-	if obj.Finance.Balance != 200 {
-		t.Error("decode 0 finance.balance test failed")
-	}
-}
-
-func assertTest2(t testing.TB, obj *testobj.TestObject) {
-	if len(obj.Finance.History) != 2 {
-		t.Error("decode 2 history len mismatch")
-	}
-	if obj.Finance.History[0].Cost != 13.1415 {
-		t.Error("decode 2 history row 0 cost mismatch")
-	}
-	if obj.Finance.History[1].Cost != 164.5962 {
-		t.Error("decode 2 history row 1 cost mismatch")
-	}
-}
-
-func assertTest3(t testing.TB, obj *testobj.TestObject) {
-	if obj.Id != "xFF45" {
-		t.Error("decode 3 id test failed")
-	}
-	if obj.Cost != 123 {
-		t.Error("decode 3 cost mismatch")
-	}
-}
-
-func assertTest4(t testing.TB, obj *testobj.TestObject) {
-	if !bytes.Equal(obj.Name, crc32Expect) {
-		t.Error("decode 4 id test failed")
-	}
-}
-
-func TestDecode0(t *testing.T) {
-	pretest(t)
-	obj := &testobj.TestObject{}
+func testDecoder(t *testing.T, jsonKey string, assertFn func(t testing.TB, obj *testobj.TestObject)) {
 	ctx := NewCtx()
-	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-	_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-	if err != nil {
-		t.Error(err)
+	obj := &testobj.TestObject{}
+	obj = assertDecode(t, ctx, obj, jsonKey)
+	assertFn(t, obj)
+}
+
+func BenchmarkDecoder(b *testing.B) {
+	b.Run("decoder1", func(b *testing.B) { benchDecoder(b, "src", scenario1) })
+	b.Run("decoder2", func(b *testing.B) { benchDecoder(b, "src", scenario2) })
+	b.Run("decoder3", func(b *testing.B) { benchDecoder(b, "srcNested", scenario3) })
+	b.Run("decoder4", func(b *testing.B) { benchDecoder(b, "src", scenario4) })
+}
+
+func benchDecoder(b *testing.B, jsonKey string, assertFn func(t testing.TB, obj *testobj.TestObject)) {
+	ctx := NewCtx()
+	obj := &testobj.TestObject{}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		obj = assertDecode(b, ctx, obj, jsonKey)
+		assertFn(b, obj)
+		obj.Clear()
 	}
-	err = Decode("decTest0", ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	if obj.Id != "xf44e" {
-		t.Error("decode 0 id test failed")
-	}
-	if !bytes.Equal(obj.Name, []byte("Marquis Warren")) {
-		t.Error("decode 0 name test failed")
-	}
-	if obj.Status != 67 {
-		t.Error("decode 0 status test failed")
-	}
-	if obj.Cost != 164.5962 {
-		t.Error("decode 0 cost test failed")
-	}
-	if obj.Finance.AllowBuy != true {
-		t.Error("decode 0 finance.allowBuy test failed")
-	}
-	if obj.Flags["read"] != 4 {
-		t.Error("decode 0 flags[read] test failed")
-	}
-	if obj.Flags["write"] != 8 {
-		t.Error("decode 0 flags[write] test failed")
-	}
+}
+
+func scenario0(t testing.TB, obj *testobj.TestObject) {
+	assertS(t, "Id", obj.Id, "xf44e")
+	assertB(t, "Name", obj.Name, []byte("Marquis Warren"))
+	assertI32(t, "Status", obj.Status, 67)
+	assertF64(t, "Cost", obj.Cost, 164.5962)
+	assertB1(t, "Finance.AllowBuy", obj.Finance.AllowBuy, true)
+	assertI32(t, "Flags[read]", obj.Flags["read"], 4)
+	assertI32(t, "Flags[write]", obj.Flags["write"], 8)
 	perm := obj.Permission
-	if (*perm)[45] != false {
-		t.Error("decode 0 permission[45] test failed")
-	}
-	if (*perm)[59] != true {
-		t.Error("decode 0 permission[45] test failed")
-	}
+	assertB1(t, "Permission[45]", (*perm)[45], false)
+	assertB1(t, "Permission[59]", (*perm)[59], true)
 }
 
-func TestDecode1(t *testing.T) {
-	pretest(t)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-	_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-	if err != nil {
-		t.Error(err)
-	}
-	err = Decode("decTest1", ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	assertTest1(t, obj)
+func scenario1(t testing.TB, obj *testobj.TestObject) {
+	assertS(t, "Id", obj.Id, "xf44e")
+	assertB(t, "Name", obj.Name, []byte("Marquis Warren"))
+	assertI32(t, "Status", obj.Status, 67)
+	assertF64(t, "Cost", obj.Cost, 164.5962)
+	assertB1(t, "Finance.AllowBuy", obj.Finance.AllowBuy, true)
+	assertF64(t, "Finance.MoneyIn", obj.Finance.MoneyIn, 15.4532)
+	assertF64(t, "Finance.MoneyOut", obj.Finance.MoneyOut, 45.90421)
+	assertF64(t, "Finance.Balance", obj.Finance.Balance, 200)
 }
 
-func TestDecode2(t *testing.T) {
-	pretest(t)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-	_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-	if err != nil {
-		t.Error(err)
-	}
-	err = Decode("decTest2", ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	assertTest2(t, obj)
+func scenario2(t testing.TB, obj *testobj.TestObject) {
+	assertI32(t, "len(Finance.History)", int32(len(obj.Finance.History)), 2)
+	assertF64(t, "Finance.History[0].Cost", obj.Finance.History[0].Cost, 13.1415)
+	assertF64(t, "Finance.History[1].Cost", obj.Finance.History[1].Cost, 164.5962)
 }
 
-func TestDecode3(t *testing.T) {
-	pretest(t)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-	_, err := ctx.SetVector("jso", decTestNestedJsonSrc, VectorJson)
-	if err != nil {
-		t.Error(err)
-	}
-	err = Decode("decTest3", ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	assertTest3(t, obj)
+func scenario3(t testing.TB, obj *testobj.TestObject) {
+	assertS(t, "Id", obj.Id, "xFF45")
+	assertF64(t, "Cost", obj.Cost, 123)
 }
 
-func TestDecode4(t *testing.T) {
-	pretest(t)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-	_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-	if err != nil {
-		t.Error(err)
-	}
-	err = Decode("decTest4", ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	assertTest4(t, obj)
-}
-
-func BenchmarkDecode1(b *testing.B) {
-	pretest(b)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-		_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-		if err != nil {
-			b.Error(err)
-		}
-		err = Decode("decTest1", ctx)
-		if err != nil {
-			b.Error(err)
-		}
-		assertTest1(b, obj)
-		ctx.Reset()
-		obj.Clear()
-	}
-}
-
-func BenchmarkDecode2(b *testing.B) {
-	pretest(b)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-		_, err := ctx.SetVector("jso", decTestSrc, VectorJson)
-		if err != nil {
-			b.Error(err)
-		}
-		err = Decode("decTest2", ctx)
-		if err != nil {
-			b.Error(err)
-		}
-		assertTest2(b, obj)
-		ctx.Reset()
-		obj.Clear()
-	}
-}
-
-func BenchmarkDecode3(b *testing.B) {
-	pretest(b)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		buf = append(buf[:0], decTestNestedJsonSrc...)
-
-		ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-		_, err := ctx.SetVector("jso", buf, VectorJson)
-		if err != nil {
-			b.Error(err)
-		}
-		err = Decode("decTest3", ctx)
-		if err != nil {
-			b.Error(err)
-		}
-		assertTest3(b, obj)
-		ctx.Reset()
-		obj.Clear()
-	}
-}
-
-func BenchmarkDecode4(b *testing.B) {
-	pretest(b)
-	obj := &testobj.TestObject{}
-	ctx := NewCtx()
-	b.ReportAllocs()
-
-	for i := 0; i < b.N; i++ {
-		buf = append(buf[:0], decTestSrc...)
-
-		ctx.Set("obj", obj, &testobj_ins.TestObjectInspector{})
-		_, err := ctx.SetVector("jso", buf, VectorJson)
-		if err != nil {
-			b.Error(err)
-		}
-		err = Decode("decTest4", ctx)
-		if err != nil {
-			b.Error(err)
-		}
-		assertTest4(b, obj)
-		ctx.Reset()
-		obj.Clear()
-	}
+func scenario4(t testing.TB, obj *testobj.TestObject) {
+	assertB(t, "Id", obj.Name, []byte(`2677594116`))
 }
