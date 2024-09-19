efa1c8653be8d5e22f546d9dcc63e6d050a72d4f
Konstantin Voykov <koykov0@gmail.com>
2024-09-19 22:47:12 +0300

    Cover continue operator with tests.

--- parser_test.go
+++ parser_test.go
@@ -13,10 +13,12 @@ func TestParser(t *testing.T) {
 	t.Run("v2c0", testParser)
 	t.Run("v2ci0", testParser)
 	t.Run("cb0", testParser)
+
 	t.Run("loop_counter", testParser)
 	t.Run("loop_range", testParser)
 	t.Run("loop_break", testParser)
 	t.Run("loop_lazybreak", testParser)
+	t.Run("loop_continue", testParser)
 }
 
 func testParser(t *testing.T) {
