1c67396a75e437c36aaa1a45d13d78957fe726bd
Konstantin Voykov <koykov0@gmail.com>
2025-06-24 08:44:54 +0300

    type assert: assign callbacks

--- assign.go
+++ assign.go
@@ -7,56 +7,56 @@ import (
 
 // AssignVectorNode implements assign callback to convert vector.Node to destination with arbitrary type.
 func AssignVectorNode(dst, src any, _ inspector.AccumulativeBuffer) (ok bool) {
-	switch src.(type) {
+	switch x := src.(type) {
 	case *vector.Node:
-		n := src.(*vector.Node)
+		n := x
 		if n.Type() == vector.TypeNull {
 			return
 		}
 		ok = true
-		switch dst.(type) {
+		switch y := dst.(type) {
 		case *[]byte:
-			*dst.(*[]byte) = n.Bytes()
+			*y = n.Bytes()
 		case *string:
-			*dst.(*string) = n.String()
+			*y = n.String()
 		case *bool:
-			*dst.(*bool) = n.Bool()
+			*y = n.Bool()
 		case *int:
 			i, _ := n.Int()
-			*dst.(*int) = int(i)
+			*y = int(i)
 		case *int8:
 			i, _ := n.Int()
-			*dst.(*int8) = int8(i)
+			*y = int8(i)
 		case *int16:
 			i, _ := n.Int()
-			*dst.(*int16) = int16(i)
+			*y = int16(i)
 		case *int32:
 			i, _ := n.Int()
-			*dst.(*int32) = int32(i)
+			*y = int32(i)
 		case *int64:
 			i, _ := n.Int()
-			*dst.(*int64) = i
+			*y = i
 		case *uint:
 			u, _ := n.Uint()
-			*dst.(*uint) = uint(u)
+			*y = uint(u)
 		case *uint8:
 			u, _ := n.Uint()
-			*dst.(*uint8) = uint8(u)
+			*y = uint8(u)
 		case *uint16:
 			u, _ := n.Uint()
-			*dst.(*uint16) = uint16(u)
+			*y = uint16(u)
 		case *uint32:
 			u, _ := n.Uint()
-			*dst.(*uint32) = uint32(u)
+			*y = uint32(u)
 		case *uint64:
 			u, _ := n.Uint()
-			*dst.(*uint64) = u
+			*y = u
 		case *float32:
 			f, _ := n.Float()
-			*dst.(*float32) = float32(f)
+			*y = float32(f)
 		case *float64:
 			f, _ := n.Float()
-			*dst.(*float64) = f
+			*y = f
 		default:
 			ok = false
 		}
